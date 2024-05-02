af678e0fd8b8190c28f8b45bd82317de41f6681d
taylosstls <luisgustavogto@gmail.com>
2024-05-02 18:15:01 -0300

    Function to add Expense item

--- mod-1/js-module/project-refund/js/scripts.js
+++ mod-1/js-module/project-refund/js/scripts.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', () => {
+  const form = document.querySelector('form')
+  
   const amount = document.querySelector('#amount')
+  const expense = document.querySelector('#expense')
+  const category = document.querySelector('#category')
 
+  const expenseList = document.querySelector('aside ul')
+
+  // Formatar valor
   amount.oninput = () => {
     let value = amount.value.replace(/\D/g, '')
 
@@ -8,6 +15,48 @@ window.addEventListener('load', () => {
 
     amount.value = formatCurrencyBRL(value)
 
+  }
+
+  // Captura o envio de submit e faz o prevent
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    // Cria o objeto com os detalhes de despesa
+    const newExpense = {
+      id: new Date().getTime(),
+      expense: expense.value,
+      category_id: category.value,
+      category_name: category.options[category.selectedIndex].text,
+      amount: amount.value,
+      created_at: new Date()
+    }
+
+    expenseAdd(newExpense)
+
+    form.reset()
+  
+  })
+
+  function expenseAdd(newExpense) {
+    try {
+      const expenseItem = `
+        <li class="expense">
+          <img src="./img/${newExpense.category_id}.svg" alt="Ícone de ${newExpense.category_name}" />
+          <div class="expense-info">
+            <strong>${newExpense.expense}</strong>
+            <span>${newExpense.category_name}</span>
+          </div>
+          <span class="expense-amount"><small>R$</small>${newExpense.amount.replace('R$', '').trim()}</span>
+          <img src="./img/remove.svg" alt="remover" class="remove-icon" />
+        </li>
+      `
+
+      expenseList.innerHTML += expenseItem
+
+    } catch (error) {
+      alert('Não foi possível atualizar a lista de despesas')
+      console.log(error)
+    }
   }
 
   function formatCurrencyBRL(value) {
