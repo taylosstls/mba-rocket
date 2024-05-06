6044dee04417bb0fb41a876bf3a92e184f045b02
taylosstls <luisgustavogto@gmail.com>
2024-05-06 11:54:54 -0300

    Create webpack bundler module

---
+++ mod-1/js-module/classroom/webpack-bundler/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";!function(t){const e=document.createElement("h1");e.textContent="Hello World!",document.body.appendChild(e)}()})();
---
+++ mod-1/js-module/classroom/webpack-bundler/src/js/index.js
@@ -0,0 +1,3 @@
+import { title } from "./components.js"
+
+title("Hello World!")
