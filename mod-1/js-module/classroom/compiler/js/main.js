2e3c8250c1d6eabb7fad752c1e0af8d125d2f26f
taylosstls <luisgustavogto@gmail.com>
2024-05-06 10:55:29 -0300

    Dist file compile modified

--- mod-1/js-module/classroom/compiler/js/main.js
+++ mod-1/js-module/classroom/compiler/js/main.js
@@ -2,6 +2,11 @@ class User {
   constructor({ email }){
     this.email = email
   }
+
+  sendMessage() {
+    console.log("Mensagem enviada para", this.email)
+  }
 }
 
 let user = new User({ email: 'luisgustavogto@gmail.com'})
+user.sendMessage()
