23a0f7f0722102e1bc18d474ae761cb3ff2804d8
taylosstls <luisgustavogto@gmail.com>
2024-05-07 17:27:29 -0300

    Add conditional to cancel a schedule day

--- mod-1/js-module/project-hairday/src/modules/schedules/cancel.js
+++ mod-1/js-module/project-hairday/src/modules/schedules/cancel.js
@@ -1,18 +1,23 @@
+import { schedulesDay } from "./load.js" 
+import { scheduleCancel } from "../../services/schedule-cancel.js"
+
 const periods = document.querySelectorAll('.period')
 
 // Gera evento click para cada evento de lista (manhã / tarde / noite)
 
 periods.forEach((period) => {
-  period.addEventListener('click', (event) => {
+  period.addEventListener('click', async (event) => {
     if(event.target.classList.contains('cancel-icon')) {
-      // Obtém a li pai do elemento clicado.
-      const item = event.target.closest("li")
-      const { id } = item.dataset
+      const item = event.target.closest("li") // Obtém a li pai do elemento clicado.
+      const { id } = item.dataset //Pega o ID do dataset
 
       if (id) {
         const isConfirm = confirm("Tem certeza que deseja cancelar o agendamento?")
 
-        if(isConfirm) console.log("REMOVER da API e deletar agendamento")
+        if(isConfirm) {
+          await scheduleCancel({ id })
+          schedulesDay() // Recarrega a lista de agendamentos
+        }
       }
     }
   })
---
+++ mod-1/js-module/project-hairday/src/services/schedule-cancel.js
@@ -0,0 +1,14 @@
+import { apiConfig } from './api-config.js'
+
+export async function scheduleCancel({ id }) {
+  try {
+    await fetch(`${apiConfig.baseURL}/schedules/${id}`, {
+      method: 'DELETE',
+    })
+
+    alert("Agendamento cancelado com sucesso!")
+  } catch (err) {
+    console.log(err)
+    alert("Não foi possível cancelar o agendamento.")
+  }
+}
