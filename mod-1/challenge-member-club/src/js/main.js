2081568562813ac45526d5288eb0331351da00eb
taylosstls <luisgustavogto@gmail.com>
2024-06-15 21:41:15 -0300

    Finish Layout Member Club

---
+++ mod-1/challenge-member-club/src/js/main.js
@@ -0,0 +1,5 @@
+"use strict"
+
+// CSS
+import "../styles/index.css"
+
---
+++ mod-1/challenge-member-club/src/services/client-info.js
@@ -0,0 +1,20 @@
+import { apiConfig } from "./api-config";
+
+
+export async function clientInfo({ id }) {
+    if (!id) throw new Error('ID is required');
+
+    try {
+        const clientURL = `${apiConfig.baseURL}/clients/${id}`;
+
+        const response = await fetch(clientURL)
+        const data = await response.json();
+
+        if (!data.id) throw new Error('Client not found in database')
+
+        return data;
+    } catch (error) {
+        console.log(error)
+        alert('Cliente não encontrado')
+    }
+}
