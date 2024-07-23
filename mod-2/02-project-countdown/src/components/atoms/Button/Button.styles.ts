b715e6b00255ef740dfcfa8e8cc314815a0b654a
taylosstls <luisgustavogto@gmail.com>
2024-07-23 20:14:55 -0300

    Create Theme version

--- mod-2/02-project-countdown/src/components/atoms/Button/Button.styles.ts
+++ mod-2/02-project-countdown/src/components/atoms/Button/Button.styles.ts
@@ -17,7 +17,10 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${({ $variant }) => css`
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+
+  /* ${({ $variant }) => css`
     background-color: ${buttonVariants[$variant]};
-  `}
+  `} */
 `;
