20777230a50ed90b15dd71eab425b3f39e0f85cb
taylosstls <luisgustavogto@gmail.com>
2024-08-16 22:36:48 -0300

    Transform Summary em useMemo conditional and add fetchTransactions as dependency on useEffect

--- mod-2/02-dt-money/src/hooks/useSummary.ts
+++ mod-2/02-dt-money/src/hooks/useSummary.ts
@@ -1,24 +1,28 @@
+import { useMemo } from "react";
+
 import { formatCurrency } from "../utils/formatter";
 
 import { Transaction } from "../contexts/TransactionsContext";
 
 export function useSummary(transactions: Transaction[]) {
-  const { income, outcome, total } = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-      } else if (transaction.type === "outcome") {
-        acc.outcome += transaction.price;
+  const { income, outcome, total } = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === "income") {
+          acc.income += transaction.price;
+        } else if (transaction.type === "outcome") {
+          acc.outcome += transaction.price;
+        }
+        acc.total = acc.income - acc.outcome;
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
       }
-      acc.total = acc.income - acc.outcome;
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+    )
+  }, [transactions])
 
   return {
     income: formatCurrency(income),
