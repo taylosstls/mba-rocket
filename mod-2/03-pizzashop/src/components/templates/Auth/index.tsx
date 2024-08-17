46c889b7c020567bbc698d8ba54df599f0794d66
Gustavo Teixeira <luisgustavogto@gmail.com>
2024-08-17 17:43:04 -0300

    Begin page auth model

--- mod-2/03-pizzashop/src/components/templates/Auth/index.tsx
+++ mod-2/03-pizzashop/src/components/templates/Auth/index.tsx
@@ -2,7 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export function AuthTemplate() {
   return (
-    <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground"></div>
       <h1>Autenticação</h1>
 
       <div>
