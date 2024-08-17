49cea6484d06187aaaf6f868d7875ad9ddcce383
Gustavo Teixeira <luisgustavogto@gmail.com>
2024-08-17 15:36:20 -0300

    Create pizzashop dashboard main base

---
+++ mod-2/03-pizzashop/src/App.tsx
@@ -0,0 +1,10 @@
+import '@/styles/global.css'
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
+  )
+}
