8f2023d60984d8fb40032268acee2688051b3f34
taylosstls <luisgustavogto@gmail.com>
2024-09-13 11:35:46 -0300

    Create error page

--- mod-2/03-pizzashop/src/routes.tsx
+++ mod-2/03-pizzashop/src/routes.tsx
@@ -2,16 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { AppTemplate } from "@/components/templates/App";
 import { AuthTemplate } from "@/components/templates/Auth";
+import { PageNotFound } from "@/pages/404";
 import { Dashboard } from "@/pages/app/Dashboard";
+import { Orders } from "@/pages/app/Orders";
 import { SignIn } from "@/pages/auth/SignIn";
-
-import { Orders } from "./pages/app/Orders";
-import { SignUp } from "./pages/auth/SignUp";
+import { SignUp } from "@/pages/auth/SignUp";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppTemplate />,
+    errorElement: <PageNotFound />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
