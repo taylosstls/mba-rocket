b1c9a8056802583f197988229de37742ac7a6d5f
Gustavo Teixeira <luisgustavogto@gmail.com>
2025-01-20 20:43:01 -0300

    Add debug in the component HTML

--- mod-2/03-pizzashop/src/components/atoms/order-status.spec.tsx
+++ mod-2/03-pizzashop/src/components/atoms/order-status.spec.tsx
@@ -6,6 +6,8 @@ describe('Order Status', () => {
   it('should display the right text when order status is pending', () => {
     const wrapper = render(<OrderStatus status="pending" />)
 
+    //wrapper.debug()
+
     const statusText = wrapper.getByText('Pendente')
     const badgeElement = wrapper.getByTestId('badge')
 
@@ -15,6 +17,8 @@ describe('Order Status', () => {
 
   it('should display the right text when order status is canceled', () => {
     const wrapper = render(<OrderStatus status="canceled" />)
+
+    //wrapper.debug()
 
     const statusText = wrapper.getByText('Cancelado')
     const badgeElement = wrapper.getByTestId('badge')
@@ -26,6 +30,8 @@ describe('Order Status', () => {
   it('should display the right text when order status is delivering', () => {
     const wrapper = render(<OrderStatus status="delivering" />)
 
+    //wrapper.debug()
+
     const statusText = wrapper.getByText('Em entrega')
     const badgeElement = wrapper.getByTestId('badge')
 
@@ -36,6 +42,8 @@ describe('Order Status', () => {
   it('should display the right text when order status is processing', () => {
     const wrapper = render(<OrderStatus status="processing" />)
 
+    //wrapper.debug()
+
     const statusText = wrapper.getByText('Em preparo')
     const badgeElement = wrapper.getByTestId('badge')
 
@@ -45,6 +53,8 @@ describe('Order Status', () => {
 
   it('should display the right text when order status is delivered', () => {
     const wrapper = render(<OrderStatus status="delivered" />)
+
+    //wrapper.debug()
 
     const statusText = wrapper.getByText('Entregue')
     const badgeElement = wrapper.getByTestId('badge')
