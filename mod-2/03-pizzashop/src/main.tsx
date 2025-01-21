ba0f65a96783165e761313e50631fb776bf53772
Gustavo Teixeira <luisgustavogto@gmail.com>
2025-01-20 22:05:24 -0300

    Setup do MSW

--- mod-2/03-pizzashop/src/main.tsx
+++ mod-2/03-pizzashop/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { App } from "@/app";
+import { enableMSW } from "./api/mocks";
+import { App } from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+enableMSW().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+})
