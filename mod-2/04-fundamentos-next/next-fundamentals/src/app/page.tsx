7f0d2f6d0ecaf1a398b085c23356e3b3bf6ab158
taylosstls <luis.teixeira@globalhitss.com.br>
2025-01-27 20:53:30 -0300

    Fetch de dados nos componentes

--- mod-2/04-fundamentos-next/next-fundamentals/src/app/page.tsx
+++ mod-2/04-fundamentos-next/next-fundamentals/src/app/page.tsx
@@ -1,5 +1,8 @@
-export default function Home() {
-  return (
-    <h1>Configuração Inicial</h1>
-  );
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const response = await fetch("https://api.github.com/users/taylosstls");
+  const user = await response.json();
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>;
 }
