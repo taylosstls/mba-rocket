dc4f1fb38726904aeb9b6b6ad2c06dfb20236fac
Gustavo Teixeira <luisgustavogto@gmail.com>
2025-03-22 19:34:01 -0300

    Configurando banco de testes

--- backend/02-api-rest-nodejs/test/transactions.spec.ts
+++ backend/02-api-rest-nodejs/test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -11,6 +12,11 @@ describe('Rotas de transação', () => {
     await app.close()
   })
 
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
   it('O usuário deve ser capaz de conseguir criar uma requisição', async () => {
     await request(app.server)
       .post('/transactions')
@@ -20,5 +26,29 @@ describe('Rotas de transação', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('Deve ser capaz de listar todas as transações', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Nova transação',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') || []
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Nova transação',
+        amount: 5000,
+      }),
+    ])
   })
 })
