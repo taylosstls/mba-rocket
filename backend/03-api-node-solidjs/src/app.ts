aa2b5101f4280b2153d87c500fe1cb9cf1265541
Gustavo Teixeira <luisgustavogto@gmail.com>
2025-03-23 16:51:28 -0300

    Configure docker and prisma

--- backend/03-api-node-solidjs/src/app.ts
+++ backend/03-api-node-solidjs/src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Gustavo Teixeira',
+    email: 'luisgustavogto@gmail.com',
+  },
+})
+
+// ORM - Object Relational Mapper
